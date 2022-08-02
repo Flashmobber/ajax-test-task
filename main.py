237a68359e1ebcc605c424c6fb887dba5fa3a570
Flashmobber <ufinaid@gmail.com>
2022-08-02 22:52:52 +0300

    removed excess

--- main.py
+++ main.py
@@ -3,11 +3,10 @@ import datetime
 from parsing.parsing_bytes import (
     get_data_from_payload_1,
     get_data_from_payload_2,
-    set_data_into_payload,
 )
 
 if __name__ == "__main__":
-    iterations = 1000
+    iterations = 1  # change this value to measure performance
     payload = "10FA0E00"
     start = datetime.datetime.now()
     for _ in range(iterations):
@@ -20,18 +19,3 @@ if __name__ == "__main__":
         get_data_from_payload_2(payload)
     print(payload, get_data_from_payload_2(payload))
     print(datetime.datetime.now() - start)
-
-    set_data_into_payload(
-        {
-            "field1": "High",
-            "field2": "01",
-            "field3": "00",
-            "field4": "60",
-            "field5": "01",
-            "field6": "01",
-            "field7": "00",
-            "field8": "Medium",
-            "field9": "00",
-            "field10": "01",
-        }
-    )
